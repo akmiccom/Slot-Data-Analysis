9ce762dd7a9d4c8981d5df34ee4c529bedb581de
makoto_yaguchi <akmiccom@gmail.com>
2025-11-19 15:14:03 +0900

    test fetch_halls for streamlit

--- app/Slot_Data_Analysis.py
+++ app/Slot_Data_Analysis.py
@@ -83,6 +83,10 @@ st.subheader("Streamlit Widgets Sample", divider="rainbow")
 tab1, tab2, tab3 = st.tabs(["概要", "詳細", "その他"])
 with tab1:
     st.markdown("ここには概要を表示します。")
+    df = fetch_halls()
+    halls = df.name.tolist()
+    for hall in halls:
+        st.markdown(f"- {hall}")
 with tab2:
     st.markdown("ここには詳細を表示します。")
 with tab3:
