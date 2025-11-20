8f7d531df6c734643da86bcc1c2f6424f49e04dd
makoto_yaguchi <akmiccom@gmail.com>
2025-11-21 08:31:42 +0900

    98_test.py テストコッミト

--- app/pages/98_test02.py
+++ app/pages/98_test02.py
@@ -3,10 +3,16 @@ import pandas as pd
 import streamlit as st
 import os
 
-url = os.environ.get("SUPABASE_URL")
-key = os.environ.get("SUPABASE_ANON_KEY")
-# key = os.environ.get("SUPABASE_SERVICE_ROLE_KEY")
-supabase = create_client(url, key)
+def get_supabase_client():
+    """supabese のクライアントを取得"""
+    url = os.environ.get("SUPABASE_URL")
+    key = os.environ.get("SUPABASE_ANON_KEY")
+    # key = os.environ.get("SUPABASE_SERVICE_ROLE_KEY")
+    if not url or not key:
+        raise RuntimeError("SUPABASE_URL / SUPABASE_ANON_KEY が設定されていません。")
+    return create_client(url, key)
+
+supabase = get_supabase_client()
 
 res = (
     supabase
